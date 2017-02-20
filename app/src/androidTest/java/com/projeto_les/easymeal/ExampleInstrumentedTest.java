7953c0f8ffdc95d1fd846175ab057ad9f8822224
Michelly Menezes <martha.michelly@gmail.com>
2017-02-20 11:19:48 -0300

    correção de package / projetoLes -> projeto_les

--- app/src/androidTest/java/com/projeto_les/easymeal/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/projeto_les/easymeal/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.projetoLes.easymeal;
+package com.projeto_les.easymeal;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.projetoLes.easymeal", appContext.getPackageName());
+        assertEquals("com.projeto_les.easymeal", appContext.getPackageName());
     }
 }
