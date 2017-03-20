4710e81a7396043e2f3c5e3a467bf597ee72ef1f
samirsm <samir.silva.medeiros@ccc.ufcg.edu.br>
2017-03-20 13:04:18 -0300

    desabilitando o teclado quando inicia o app

--- app/src/main/java/com/projeto_les/easymeal/fragments/SelectIngredientsFragment.java
+++ app/src/main/java/com/projeto_les/easymeal/fragments/SelectIngredientsFragment.java
@@ -338,7 +338,6 @@ public class SelectIngredientsFragment extends Fragment {
                 addIngredientAux();
             }
         });
-
         mIngredientEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
